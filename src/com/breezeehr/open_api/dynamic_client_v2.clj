bb9eada6feb52cc7c73a1c89426cbe14e2241e02
drew verlee <drew.verlee@gmail.com>
2020-06-10 10:17:56 -0500

    add a comment that shows data passed to make-method

--- src/com/breezeehr/open_api/dynamic_client_v2.clj
+++ src/com/breezeehr/open_api/dynamic_client_v2.clj
@@ -14,3 +14,26 @@
                                       :body
                                       :paths)]
     (dc/make-method base-url parameters path)))
+(comment
+  (def client (create-client {:base-url "http://127.0.0.1:8001" :version "v2"}))
+
+
+
+
+  ;; for reference this is what the first arg to make-method looks like
+  (first @dc/d)
+;; => ["http://127.0.0.1:8001"
+;;     [{"uniqueItems" true,
+;;       "type" "string",
+;;       "description" "name of the APIService",
+;;       "name" "name",
+;;       "in" "path",
+;;       "required" true}
+;;      {"uniqueItems" true,
+;;       "type" "string",
+;;       "description" "If 'true', then the output is pretty printed.",
+;;       "name" "pretty",
+;;       "in" "query"}]
+;;     "/apis/apiregistration.k8s.io/v1beta1/apiservices/{name}/status"]
+
+  )
