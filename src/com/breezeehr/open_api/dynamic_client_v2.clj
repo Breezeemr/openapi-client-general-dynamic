0e392aed2bd424b5bc4f31361b90998eabf68c9e
drew verlee <drew.verlee@gmail.com>
2020-06-10 10:18:19 -0500

    correctly loop on paths then config filtering out the parameter key

--- src/com/breezeehr/open_api/dynamic_client_v2.clj
+++ src/com/breezeehr/open_api/dynamic_client_v2.clj
@@ -1,19 +1,26 @@
 (ns com.breezeehr.open-api.dynamic-client-v2
   (:require [com.breezeehr.open-api.dynamic-client :as dc]
-            [aleph.http :as http]
-            ))
+            [aleph.http :as http]))
+
+(defn make-method-v2
+  [& args] args)
 
 (defn create-client
   [{:keys [base-url version]}]
-  (for [[path {:keys [parameters]}] (->> {:method           :get
-                                          :url              (str base-url "/openapi/" version)
-                                          :throw-exceptions false
-                                          :as               :json}
-                                      http/request
-                                      deref
-                                      :body
-                                      :paths)]
-    (dc/make-method base-url parameters path)))
+  (for [[path {:keys [parameters] :as config}] (->> {:method           :get
+                                                     :url              (str base-url "/openapi/" version)
+                                                     :throw-exceptions false
+                                                     :as               :json}
+                                                 http/request
+                                                 deref
+                                                 :body
+                                                 :paths)
+        ;;TODO better names
+        [k v] config :when (not= k :parameters)]
+    ;;[base-url parameters path]
+    ;;TODO make-method-v2
+    (make-method-v2 base-url parameters path)))
+
 (comment
   (def client (create-client {:base-url "http://127.0.0.1:8001" :version "v2"}))
 
